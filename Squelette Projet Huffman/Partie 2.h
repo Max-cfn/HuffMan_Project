623dfb9f8d7ca2e032a9b36e6ad0e8f2afd38291
Bordier-Loann <72803132+Bordier-Loann@users.noreply.github.com>
2020-12-10 15:04:28 +0100

    Update Partie 2.h

--- Squelette Projet Huffman/Partie 2.h
+++ Squelette Projet Huffman/Partie 2.h
@@ -2,7 +2,7 @@
 #define PARTIE_2_H_INCLUDED
 
 typedef struct Element{
-    int occurence;
+    int occurrence;
     int lettre;
     struct Element* next;
 }Element;
@@ -21,15 +21,16 @@ extern void print_tree(Tree a);
 extern void add_1_or_0(char* c, int nbr);
 extern void reset_chain(char* c);
 extern void dico_huffman(Tree a, Element* l);
+extern void encoding();
 int maximum_letter(Element* l);
-int maximum_occurence(Element* l);
+int maximum_occurrence(Element* l);
 int size_list(Element *l);
-int recup_occurence(Element *l);
-int nbr_occurence(Element* l);
-int fill_chain(char* c, Tree a, Element* l);
+int recup_occurrence(Element *l);
+int nbr_occurrence(Element* l);
+int fill_chain(char* c, Tree a, char letter);
 Element* create_elem_List(int lettre);
-Element* creat_list_letter();
-Element* supp_element(Element** l, int maxi);
+Element* create_list_letter();
+Element* supp_element(Element** l, int occurrence, char letter);
 Element* create_element(int nouv_info);
 Node* create_node(int occur, char letter);
 Node* tree_huffman(Element* l);
