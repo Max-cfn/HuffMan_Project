32a6446f394042019318faf3f5c443970612890c
Max-cfn <73079596+Max-cfn@users.noreply.github.com>
2020-12-11 20:04:32 +0100

    Add files via upload

---
+++ Partie 1.h
@@ -0,0 +1,12 @@
+#ifndef PARTIE_1_H_INCLUDED
+#define PARTIE_1_H_INCLUDED
+
+
+void count_number();
+void translate();
+int Saisie_securisee_menu();
+void purge(void);
+
+#endif // PARTIE_1_H_INCLUDED
+
+
---
+++ Partie 3.h
@@ -0,0 +1,9 @@
+#ifndef PARTIE_3_H_INCLUDED
+#define PARTIE_3_H_INCLUDED
+#include "Partie 2.h"
+
+Node* create_tab_occurrence();
+extern void print_tab(Node* tab);
+
+
+#endif // PARTIE_3_H_INCLUDED
